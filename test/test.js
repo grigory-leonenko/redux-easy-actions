8c1d94c95612725f2c8f9ae14ef77eb59de6faee
Grigory Leonenko <grigory.leonenko@gmail.com>
2016-01-22 18:12:55 +0400

    Cover new core with tests.

--- test/test.js
+++ test/test.js
@@ -2,110 +2,26 @@ import EasyActions from '../src/core.js';
 import assert from 'assert';
 
 describe('redux-easy-actions full class decorator tests', () => {
-    @EasyActions()
-    class Actions {
-        constructor(dispatcher){
-            this.dispatcher = dispatcher;
+    const ItemActions = EasyActions({
+        ADD_ITEM(type, text){
+            return {type, text}
         }
-        ADD_ITEM(type, item){
-            return {type, item}
-        }
-        DELETE_ITEM(type, id){
-            return {type, id}
-        }
-        LINKED_ACTION(type, id){
-            return this.DELETE_ITEM(id)
-        }
-        DISPATCH_ACTION(){
-            return this.dispatcher;
-        }
-    }
+    })
+    const {Actions, Constants} = ItemActions;
 
-    it('new actions instance creating', () => {
-        let a = new Actions();
-        assert(typeof a === 'object');
+    it('constants and actions created', () => {
+        assert(Actions && Constants);
     });
 
-    it('right action type from function method', () => {
-        let a = new Actions();
-        assert(Actions.ADD_ITEM === 'ADD_ITEM' && a.DELETE_ITEM.type === 'DELETE_ITEM')
-    })
-
-    it('right action type inside payload', () => {
-        let a = new Actions();
-        assert(a.ADD_ITEM().type === 'ADD_ITEM')
+    it('right action type from action', () => {
+        assert(Actions.ADD_ITEM().type === 'ADD_ITEM')
     })
 
     it('right payload from action', () => {
-        let a = new Actions();
-        assert(a.ADD_ITEM('FOO_ITEM').item === 'FOO_ITEM')
+        assert(Actions.ADD_ITEM('Foo').text === 'Foo')
     })
 
-    it('test context inside function', () => {
-        let a = new Actions();
-        assert(a.LINKED_ACTION(42).id === 42)
+    it('constant with action name exists', () => {
+        assert(Constants.ADD_ITEM === 'ADD_ITEM')
     })
-
-    it('get dispatcher from constructor', () => {
-        let a = new Actions('DISPATCHER');
-        assert(a.DISPATCH_ACTION() === 'DISPATCHER')
-    })
-
-})
-
-
-describe('redux-easy-actions single method decorator tests', () => {
-    class Actions {
-        constructor(dispatcher){
-            this.dispatcher = dispatcher;
-        }
-        @EasyActions(Actions)
-        ADD_ITEM(type, item){
-            return {type, item}
-        }
-        @EasyActions(Actions)
-        DELETE_ITEM(type, id){
-            return {type, id}
-        }
-        @EasyActions(Actions)
-        LINKED_ACTION(type, id){
-            return this.DELETE_ITEM(id)
-        }
-        @EasyActions(Actions)
-        DISPATCH_ACTION(){
-            return this.dispatcher;
-        }
-    }
-
-    it('new actions instance creating', () => {
-        let a = new Actions();
-        assert(typeof a === 'object');
-    });
-
-    it('right action type from function method', () => {
-        let a = new Actions();
-        console.log(Actions.ADD_ITEM)
-        assert(Actions.ADD_ITEM === 'ADD_ITEM' && a.DELETE_ITEM.type === 'DELETE_ITEM')
-    })
-
-    it('right action type inside payload', () => {
-        let a = new Actions();
-        assert(a.ADD_ITEM().type === 'ADD_ITEM')
-    })
-
-    it('right payload from action', () => {
-        let a = new Actions();
-        assert(a.ADD_ITEM('FOO_ITEM').item === 'FOO_ITEM')
-    })
-
-    it('test context inside function', () => {
-        let a = new Actions();
-        assert(a.LINKED_ACTION(42).id === 42)
-    })
-
-    it('get dispatcher from constructor', () => {
-        let a = new Actions('DISPATCHER');
-        assert(a.DISPATCH_ACTION() === 'DISPATCHER')
-    })
-
 })
